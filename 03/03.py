9c5495a76ea8b160301da89d8f2c1ea71165946c
Maxime Monge <maxime.monge@yahoo.fr>
2021-12-03 20:38:43 +0100

    Jour 3 : partie 2

--- 03/03.py
+++ 03/03.py
@@ -21,3 +21,54 @@ print("Réponse test : " + str(testOne) + " --> " + str(testOne == 198))
 
 resultOne = getPowerConsumption('03_input.txt', 12)
 print("Réponse : " + str(resultOne) + " --> " + str(resultOne == 1071734))
+
+
+def getLifeSupportRating(filename, bits):
+    oxygenGeneratorRating = 0
+    cooScrubber = 0
+    firstStepZero = []
+    firstStepOne = []
+    lines = getLines(filename)
+    for line in lines:
+        if line[0] == "1":
+            firstStepOne.append(line)
+        else:
+            firstStepZero.append(line)
+    if len(firstStepOne) > len(firstStepZero):
+        oxygenGeneratorRating = traitment(firstStepOne, True, 1)
+        cooScrubber = traitment(firstStepZero, False, 1)
+    else:
+        oxygenGeneratorRating = traitment(firstStepZero, True, 1)
+        cooScrubber = traitment(firstStepOne, False, 1)
+    return oxygenGeneratorRating * cooScrubber
+
+def traitment(liste, useMax, index):
+    ones = []
+    zeros = []
+    for element in liste:
+        if element[index] == "1":
+            ones.append(element)
+        else:
+            zeros.append(element)
+    moreOnesThanZeros = len(ones) >= len(zeros)
+    if useMax:
+        liste = ones if moreOnesThanZeros else zeros
+    else:
+        liste = zeros if moreOnesThanZeros else ones
+    if len(liste) > 1:
+        return traitment(liste, useMax, index + 1)
+    else:
+        return getDecimalValue(liste[0])
+
+def getDecimalValue(binary):
+    decimal = 0
+    for index in range(len(binary)):
+        decimal += int(binary[index]) * (2 ** (len(binary) - index - 1))
+    return decimal
+
+testTwo = getLifeSupportRating('03_input_exemple.txt', 5)
+print("Réponse test : " + str(testTwo) + " --> " + str(testTwo == 230))
+
+resultTwo = getLifeSupportRating('03_input.txt', 12)
+print("Réponse : " + str(resultTwo) + " --> " + str(resultTwo == 6124992))
+
