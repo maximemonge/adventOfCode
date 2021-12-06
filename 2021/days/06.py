26bc7ae908ef7dcb647954ec00428d65fb5ca1a6
Maxime Monge <maxime.monge@yahoo.fr>
2021-12-06 23:56:50 +0100

    Jour 6 : partie 2

--- 2021/days/06.py
+++ 2021/days/06.py
@@ -1,40 +1,40 @@
 from utils import getLines
 
-def traitement(datas, nbDays):
-    while nbDays > 0:
-        nbAppend = 0
-        for i in range(len(datas)):
-            datas[i] = int(datas[i]) - 1
-            if int(datas[i]) < 0:
-                datas[i] = 6
-                nbAppend += 1
-        while nbAppend > 0:
-            datas.append(8)
-            nbAppend -= 1
-        nbDays -= 1
-    return len(datas)
-
-def getLanternfishCount(filename, nbDays):
+def getLanternfishCount(filename, days):
     datas = getLines(filename)[0].split(",")
-    dictionnaire = {}
-    count = 0
-    for i in range(len(datas)):
+    lanternfishCounter = init()
+    for number in datas:
+        lanternfishCounter[int(number)] += 1
+    while days:
+        nbZeros = lanternfishCounter[0]
+        for i in range(len(lanternfishCounter)):
+            if i == 8:
+                lanternfishCounter[i] = nbZeros
+            else:
+                lanternfishCounter[i] = lanternfishCounter[i + 1]
+                if i == 6:
+                    lanternfishCounter[i] += nbZeros
+        days -= 1
+    return sum(lanternfishCounter.values())
 
-        if(datas[i] in dictionnaire):
-            count += dictionnaire[datas[i]]
-        else:
-            copyNbJour = nbDays
-            res = traitement([datas[i]], nbDays)
-            count += res
-            dictionnaire[datas[i]] = res
-    return count
+def init():
+    dictionnary = {}
+    for i in range(9):
+        dictionnary[i] = 0
+    return dictionnary
     
 
 testOne = getLanternfishCount('../inputs/06_input_exemple.txt', 18)
-print("Réponse test 1 partie 1 : " + str(testOne) + " --> " + str(testOne == 26))
+print("Réponse test 1 : " + str(testOne) + " --> " + str(testOne == 26))
 
 testTwo = getLanternfishCount('../inputs/06_input_exemple.txt', 80)
-print("Réponse test 2 partie 1 : " + str(testTwo) + " --> " + str(testTwo == 5934))
+print("Réponse test 2 : " + str(testTwo) + " --> " + str(testTwo == 5934))
+
+testThree = getLanternfishCount('../inputs/06_input_exemple.txt', 256)
+print("Réponse test 3 : " + str(testThree) + " --> " + str(testThree == 26984457539))
 
 resultOne = getLanternfishCount('../inputs/06_input.txt', 80)
 print("Réponse partie 1 : " + str(resultOne) + " --> " + str(resultOne == 373378))
+
+resultTwo = getLanternfishCount('../inputs/06_input.txt', 256)
+print("Réponse partie 2 : " + str(resultTwo) + " --> " + str(resultTwo == 1682576647495))
