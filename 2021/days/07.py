641e2916790fa263569e32cec48d1b254019a085
Maxime Monge <maxime.monge@yahoo.fr>
2021-12-07 18:25:47 +0100

    Jour 7 : refacto

--- 2021/days/07.py
+++ 2021/days/07.py
@@ -1,41 +1,30 @@
 from utils import getLines
 
-def getCheapestFuelCost(filename):
+def getCheapestFuelCost(filename, rule):
     datas = getLines(filename)[0].split(",")
     maximum = max(datas)
     cheapestFuelCost = -1
     for i in range(int(maximum)):
         fuel = 0
         for number in datas:
-            fuel += abs(i - int(number))
+            if rule == 1:
+                fuel += abs(i - int(number))
+            else:
+                fuel += sum(range(1, abs(i - int(number)) + 1))
         if cheapestFuelCost == -1:
             cheapestFuelCost = fuel
         elif fuel < cheapestFuelCost:
             cheapestFuelCost = fuel
     return cheapestFuelCost
 
-def getCheapestFuelCostNewRule(filename):
-    datas = getLines(filename)[0].split(",")
-    maximum = max(datas)
-    cheapestFuelCost = -1
-    for i in range(int(maximum)):
-        fuel = 0
-        for number in datas:
-            fuel += sum(range(1, abs(i - int(number)) + 1))
-        if cheapestFuelCost == -1:
-            cheapestFuelCost = fuel
-        elif fuel < cheapestFuelCost:
-            cheapestFuelCost = fuel
-    return cheapestFuelCost
-
-testOne = getCheapestFuelCost('../inputs/07_input_exemple.txt')
+testOne = getCheapestFuelCost('../inputs/07_input_exemple.txt', 1)
 print("Réponse test 1 : " + str(testOne) + " --> " + str(testOne == 37))
 
-testTwo = getCheapestFuelCostNewRule('../inputs/07_input_exemple.txt')
+testTwo = getCheapestFuelCost('../inputs/07_input_exemple.txt', 2)
 print("Réponse test 2 : " + str(testTwo) + " --> " + str(testTwo == 168))
 
-resultOne = getCheapestFuelCost('../inputs/07_input.txt')
+resultOne = getCheapestFuelCost('../inputs/07_input.txt', 1)
 print("Réponse partie 1 : " + str(resultOne) + " --> " + str(resultOne == 347449))
 
-resultTwo = getCheapestFuelCostNewRule('../inputs/07_input.txt')
+resultTwo = getCheapestFuelCost('../inputs/07_input.txt', 2)
 print("Réponse partie 2 : " + str(resultTwo) + " --> " + str(resultTwo == 98039527))
