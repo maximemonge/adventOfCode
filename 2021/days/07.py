6faa89d56524830461a70e0b10ad0a40c7335c7d
Maxime Monge <maxime.monge@yahoo.fr>
2021-12-07 18:22:09 +0100

    Jour 7 : partie 2

--- 2021/days/07.py
+++ 2021/days/07.py
@@ -13,10 +13,29 @@ def getCheapestFuelCost(filename):
         elif fuel < cheapestFuelCost:
             cheapestFuelCost = fuel
     return cheapestFuelCost
-    
+
+def getCheapestFuelCostNewRule(filename):
+    datas = getLines(filename)[0].split(",")
+    maximum = max(datas)
+    cheapestFuelCost = -1
+    for i in range(int(maximum)):
+        fuel = 0
+        for number in datas:
+            fuel += sum(range(1, abs(i - int(number)) + 1))
+        if cheapestFuelCost == -1:
+            cheapestFuelCost = fuel
+        elif fuel < cheapestFuelCost:
+            cheapestFuelCost = fuel
+    return cheapestFuelCost
 
 testOne = getCheapestFuelCost('../inputs/07_input_exemple.txt')
 print("Réponse test 1 : " + str(testOne) + " --> " + str(testOne == 37))
 
+testTwo = getCheapestFuelCostNewRule('../inputs/07_input_exemple.txt')
+print("Réponse test 2 : " + str(testTwo) + " --> " + str(testTwo == 168))
+
 resultOne = getCheapestFuelCost('../inputs/07_input.txt')
 print("Réponse partie 1 : " + str(resultOne) + " --> " + str(resultOne == 347449))
+
+resultTwo = getCheapestFuelCostNewRule('../inputs/07_input.txt')
+print("Réponse partie 2 : " + str(resultTwo) + " --> " + str(resultTwo == 98039527))
